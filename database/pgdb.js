be9d0b68ab189abbd53e650d7de549ecb522f97c
Alun Turner <alun.turner@googlemail.com>
2020-09-01 10:40:56 +0100

    course complete

--- database/pgdb.js
+++ database/pgdb.js
@@ -45,5 +45,14 @@ module.exports = (pgPool) => {
         )
         .then((res) => humps.camelizeKeys(res.rows[0]));
     },
+
+    getActivitiesForUserIds(userIds) {
+      return pgPool
+        .query(
+          `select created_by, created_at, label, '' as title, 'name' as activity_type from names where created_by = ANY($1) union select created_by, created_at, '' as label, title, 'contest' as activity_type from contests where created_by = ANY($1)`,
+          [userIds]
+        )
+        .then((res) => orderedFor(res.rows, userIds, "createdBy", false));
+    },
   };
 };
--- schema/types/user.js
+++ schema/types/user.js
@@ -7,41 +7,50 @@ const {
   GraphQLInt,
 } = require("graphql");
 
-const ContestType = require("./contest");
-
 module.exports = new GraphQLObjectType({
   name: "UserType",
-  fields: {
-    id: { type: GraphQLID },
-    firstName: { type: GraphQLString },
-    lastName: { type: GraphQLString },
-    email: { type: new GraphQLNonNull(GraphQLString) },
-    createdAt: { type: GraphQLString },
-    contests: {
-      type: new GraphQLList(ContestType),
-      resolve(obj, args, { loaders }) {
-        return loaders.contestsForUserIds.load(obj.id);
+  fields: () => {
+    const ContestType = require("./contest");
+    const ActivityType = require("./activity");
+
+    return {
+      id: { type: GraphQLID },
+      firstName: { type: GraphQLString },
+      lastName: { type: GraphQLString },
+      email: { type: new GraphQLNonNull(GraphQLString) },
+      createdAt: { type: GraphQLString },
+      contests: {
+        type: new GraphQLList(ContestType),
+        resolve(obj, args, { loaders }) {
+          return loaders.contestsForUserIds.load(obj.id);
+        },
       },
-    },
-    contestsCount: {
-      type: GraphQLInt,
-      resolve(obj, args, { loaders }, { fieldName }) {
-        return loaders.mdb.usersByIds
-          .load(obj.id)
-          .then((res) => res[fieldName]);
+      contestsCount: {
+        type: GraphQLInt,
+        resolve(obj, args, { loaders }, { fieldName }) {
+          return loaders.mdb.usersByIds
+            .load(obj.id)
+            .then((res) => res[fieldName]);
+        },
       },
-    },
-    namesCount: {
-      type: GraphQLInt,
-      resolve(obj, args, { loaders }, { fieldName }) {
-        return loaders.mdb.usersByIds.load(obj.id);
+      namesCount: {
+        type: GraphQLInt,
+        resolve(obj, args, { loaders }, { fieldName }) {
+          return loaders.mdb.usersByIds.load(obj.id);
+        },
       },
-    },
-    votesCount: {
-      type: GraphQLInt,
-      resolve(obj, args, { loaders }, { fieldName }) {
-        return loaders.mdb.usersByIds.load(obj.id);
+      votesCount: {
+        type: GraphQLInt,
+        resolve(obj, args, { loaders }, { fieldName }) {
+          return loaders.mdb.usersByIds.load(obj.id);
+        },
       },
-    },
+      activities: {
+        type: new GraphQLList(ActivityType),
+        resolve(obj, args, { loaders }) {
+          return loaders.activitiesForUserIds.load(obj.id);
+        },
+      },
+    };
   },
 });
